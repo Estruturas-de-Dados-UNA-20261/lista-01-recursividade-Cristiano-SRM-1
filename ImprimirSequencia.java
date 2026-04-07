426bc628b81cc008203f0e65a5e683e71de1ad26
Cristiano-S <57049189+Cristiano-SRM@users.noreply.github.com>
2026-04-07 22:56:01 +0000

    melhora do exercicio 2

--- ImprimirSequencia.java
+++ ImprimirSequencia.java
@@ -1,22 +1,23 @@
 import java.util.*;
 public class ImprimirSequencia {
+  static Scanner teclado = new Scanner(System.in);
   public static void inverte(int n){
-    Scanner teclado = new Scanner(System.in);
       if (n == 0){
+        System.out.println("\n");
           return;
       }
       System.out.println("Informe a sequencia numerica: ");
       int sequencia = teclado.nextInt();
       inverte(n-1);
       System.out.println(sequencia);
+      
   }
     
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
        System.out.println("Informe o tamanho da sequencia numerica: ");
-       int tamanho = input.nextInt();
+       int tamanho = teclado.nextInt();
        
        inverte(tamanho);
-       
        }
+       
     }
